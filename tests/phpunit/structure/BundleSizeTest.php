bbb03315e91268a2d3a0999ad85cb9bc39517fa2
hslater <hamish.slater@hallowelt.com>
2026-02-23 20:01:13 +0700

    Declare RL module budgets
    ERM20742
    
    [master]
    
    Change-Id: I6666482c2f7c902eb8ad6813713fc4d4e6b9bea4

---
+++ tests/phpunit/structure/BundleSizeTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace MediaWiki\Extension\UserProfile\Tests\Structure;
+
+use MediaWiki\Tests\Structure\BundleSizeTestBase;
+
+class BundleSizeTest extends BundleSizeTestBase {
+
+	/** @inheritDoc */
+	public static function getBundleSizeConfigData(): string {
+		return dirname( __DIR__, 3 ) . '/bundlesize.config.json';
+	}
+}
