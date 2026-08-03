9f7ba68b61f08d6b9c3251eca37cfcc1f196fe5e
Hamish <hamish.slater@hallowelt.com>
2026-08-03 17:00:53 +0700

    Replace deprecated Language::__construct
    ERM48053
    
    [5.1.x]
    
    Change-Id: If1b36d124d079f81e9bbf5f4f8058fd5be7f186e
    (cherry picked from commit 89f59a9ae65052adc27c637e60de74e146e856d4)

--- src/Hooks/AddTag.php
+++ src/Hooks/AddTag.php
@@ -4,6 +4,7 @@ namespace MediaWiki\Extension\UserProfile\Hooks;
 
 use MediaWiki\Extension\UserProfile\ProfileFieldRegistry;
 use MediaWiki\Extension\UserProfile\Tag\UserProfileTag;
+use MediaWiki\Languages\LanguageFactory;
 use MediaWiki\User\UserFactory;
 use MWStake\MediaWiki\Component\GenericTagHandler\Hook\MWStakeGenericTagHandlerInitTagsHook;
 
@@ -12,10 +13,12 @@ class AddTag implements MWStakeGenericTagHandlerInitTagsHook {
 	/**
 	 * @param UserFactory $userFactory
 	 * @param ProfileFieldRegistry $profileFieldRegistry
+	 * @param LanguageFactory $languageFactory
 	 */
 	public function __construct(
 		private readonly UserFactory $userFactory,
-		private readonly ProfileFieldRegistry $profileFieldRegistry
+		private readonly ProfileFieldRegistry $profileFieldRegistry,
+		private readonly LanguageFactory $languageFactory
 	) {
 	}
 
@@ -23,6 +26,6 @@ class AddTag implements MWStakeGenericTagHandlerInitTagsHook {
 	 * @inheritDoc
 	 */
 	public function onMWStakeGenericTagHandlerInitTags( array &$tags ) {
-		$tags[] = new UserProfileTag( $this->userFactory, $this->profileFieldRegistry );
+		$tags[] = new UserProfileTag( $this->userFactory, $this->profileFieldRegistry, $this->languageFactory );
 	}
 }
